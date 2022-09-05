fa3beed14aebc342a1d8f6446b9a512893a3b1ba
Daniel Jooryabi <djooryabi@gmail.com>
2022-09-05 09:13:19 -0700

    Add double jump, melee attack, and targets.

--- Assets/code_src/PlayerController.cs
+++ Assets/code_src/PlayerController.cs
@@ -1,44 +1,83 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class PlayerController : MonoBehaviour
 {
+    public float MELEE_WEAPON_ACTIVE_TIME = 0.1f;
+    public const int MAX_JUMP_COUNT = 2;
+    public const float INPUT_DEAD_ZONE = 0.1f;
+
     public float maxXSpeed;
     public float maxYSpeed;
 
     private float inputX;
-    private float inputY;
+    private bool inputY;
 
     public float jumpForce;
 
     private Rigidbody2D rb;
 
-    private bool isGrounded;
+    private int jumpCount;
 
-    // Start is called before the first frame update
+    private bool isGrounded;
+    private MeleeWeapon meleeWeapon;
+    private TimerManager timerManager;
+    private const int MELEE_TIMER = 0;
+
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        meleeWeapon = GetComponentInChildren<MeleeWeapon>();
+        meleeWeapon.gameObject.SetActive(false);
+        timerManager = new TimerManager();
     }
 
-    // Update is called once per frame
     void Update()
     {
+        timerManager.Update(Time.deltaTime);
+
         inputX = Input.GetAxisRaw("Horizontal");
-        inputY = Input.GetAxisRaw("Vertical");
+
+        if (jumpCount < MAX_JUMP_COUNT && Input.GetKeyDown(KeyCode.Space))
+        {
+            inputY = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            MeleeAttack();
+        }
+    }
+
+    private void MeleeAttack()
+    {
+        if (timerManager.ContainsTimer(MELEE_TIMER))
+        {
+            return;
+        }
+
+        meleeWeapon.gameObject.SetActive(true);
+        List<Action> actions = new List<Action>();
+        actions.Add(() => meleeWeapon.gameObject.SetActive(false));
+        ActionTimer actionTimer = new ActionTimer(MELEE_TIMER, actions, MELEE_WEAPON_ACTIVE_TIME);
+        timerManager.RegisterTimer(actionTimer);
     }
 
     private void FixedUpdate()
     {
-        if (inputX > 0.1f || inputX < -0.1f)
+
+        if (inputX > INPUT_DEAD_ZONE || inputX < -INPUT_DEAD_ZONE)
         {
             rb.AddForce(new Vector2(inputX * maxYSpeed, 0f), ForceMode2D.Impulse);
         }
 
-        if (inputY > 0.1f && isGrounded)
+        if (inputY)
         {
+            jumpCount++;
             rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+            inputY = false;
         }
 
         float currentXSpeed = rb.velocity.x;
@@ -57,6 +96,7 @@ public class PlayerController : MonoBehaviour
         if (collision.gameObject.tag == "Platform")
         {
             isGrounded = true;
+            jumpCount = 0;
         }
     }
 
