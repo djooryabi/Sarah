cf3318a5531ae6a17573b0ab8c1d13835aab0e0b
Austin Tasato <aytasato@gmail.com>
2022-09-08 01:44:35 -0700

    feat(ui): Tiny MVP for Settings Modal UI mockup
    - Also added skeleton class for
      UISettingsModal.cs

---
+++ Assets/code_src/ui/UISettingsModalManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+///
+/// Manages and exposes state for the UI Settings Modal.
+///
+/// It should be relatively drag-n-drop into any scene's top-level Canvas
+/// in order to allow it to show up.
+///
+public class UISettingsModalManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
